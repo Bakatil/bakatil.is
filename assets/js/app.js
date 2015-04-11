fd6a6b992500401952980366928c334d611e1e76
Dagur Kristinsson <dagurk@gmail.com>
2015-04-11 00:50:39 +0000

    Soft launch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
-$(document).foundation();
+$(document).foundation({
+  offcanvas : {
+    // Sets method in which offcanvas opens.
+    // [ move | overlap_single | overlap ]
+    open_method: 'move',
+    // Should the menu close when a menu link is clicked?
+    // [ true | false ]
+    close_on_click : true
+  }
+});
 
 $(function() {
 
@@ -11,13 +20,12 @@ $(function() {
     toggleHandler(toggle);
   };
 
-  var expandMenu = $('.responsive-menu');
+  //var expandMenu = $('.responsive-menu');
 
   function toggleHandler(toggle) {
     toggle.addEventListener( "click", function(e) {
       e.preventDefault();
       (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
-      (expandMenu.hasClass("expand") === true) ? expandMenu.removeClass("expand") : expandMenu.addClass("expand");
     });
   }
 
@@ -28,5 +36,8 @@ $(function() {
       mobileDeceleration: 0.004
     });
   } catch(e) {};
-
 });
+//$( window ).load(function() {
+//  $(document).foundation('interchange', 'reflow');
+//  $(document).foundation('interchange', 'resize');
+//});
